7aa88abad1d6898a1c0743c3259176a904759be2
AjiriOgeh <ajiogeh@gmail.com>
2024-05-22 07:03:58 +0100

    Initial Commit

--- src/JsonSerializer.java
+++ src/JsonSerializer.java
@@ -3,17 +3,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonSerializer {
 
-    public static String serialize(Transaction transaction) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String json = mapper.writeValueAsString(transaction);
-            return json;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
     public static Transaction[] deserialize(String json) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(json, Transaction[].class);
